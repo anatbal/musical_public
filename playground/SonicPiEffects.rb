bdfe662bfdfae6f22d4fa296796acbdf624cb330
Ido Bokobza <idobokobza@gmail.com>
2019-01-08 20:50:06 +0200

    added new sonic-pi base

--- playground/SonicPiEffects.rb
+++ playground/SonicPiEffects.rb
@@ -1,17 +1,9 @@
 live_loop :effect do
   use_real_time
   param = sync "/osc/effect"
-  mySamp = param[0]
-  with_fx :whammy, mix: param[6] do
-    with_fx :ixi_techno, mix: param[7] do
-      with_fx :bitcrusher, mix: param[8] do
-        with_fx :bpf, mix: param[9] do
-          with_fx :rhpf, mix: param[10] do
-            sample mySamp,start: param[1], finish: param[2], amp: param[4], pan: param[5]
-            sleep param[3]
-          end
-        end
-      end
-    end
+  with_fx :eq, high: param[7], low: param[8] do
+    mySamp = param[0]
+    sample mySamp,start: param[1], finish: param[2], amp: param[4], pan: param[5], release: param[6]
+    sleep param[3]
   end
 end
